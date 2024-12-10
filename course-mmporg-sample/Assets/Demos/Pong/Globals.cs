6295802982b22fb9a1963cccbe78d2a442486b0b
taneleuh <taneloubadia@gmail.com>
2024-12-10 11:06:19 +0100

    you can now choose your team

--- course-mmporg-sample/Assets/Demos/Pong/Globals.cs
+++ course-mmporg-sample/Assets/Demos/Pong/Globals.cs
@@ -4,4 +4,6 @@ public class Globals
 {
     public static bool IsServer = true;
     public static PongPlayer LocalPlayer;
+
+    public static bool teamChoice = false; //True -> Left team | False -> Right team
 }
